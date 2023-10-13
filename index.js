31845fd16fcaab22db1bd54146b4e7020d74e801
Juan Camilo Betancur Rico <camilo.betancur@trycore.com.co>
2023-10-13 14:15:21 -0500

    1. Instalación Express y Ejecución Servidor con ejemplo 'Hola Mundo'

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("My App")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo!')
+})
+
+app.listen(port, () => {
+    console.log('Escuchando en el puerto: ' + port);
+});
